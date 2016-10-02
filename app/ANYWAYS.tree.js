30f0d7665d3612058e62d5876f6dd911d4c674ef
ben.abelshausen <ben@osmsharp.com>
2016-10-02 22:18:24 +0200

    Updated animation and display of current route.

--- app/ANYWAYS.tree.js
+++ app/ANYWAYS.tree.js
@@ -13,6 +13,8 @@ ANYWAYS.tree = {
         //var color2 = '00FFFF';
         var color3 = 'FFFFFF';
 
+        me.index = new rbush();
+
         me.profile = profile;
         me.report = function (edgeId) {
             if (me.onOverEdge) {
@@ -37,11 +39,16 @@ ANYWAYS.tree = {
 
             var routeEdge = edgesMap[edgeId];
             var routeEdgeShape = [];
+            var consideredEdgeMap = {};
             while (routeEdge) {
                 for (var i = routeEdge.shape.length - 1; i >= 0; i--) {
                     routeEdgeShape.unshift(routeEdge.shape[i]);
                 }
 
+                consideredEdgeMap[routeEdge.edgeId] = true;
+                if (consideredEdgeMap[routeEdge.previousEdgeId] === true) {
+                    break;
+                }
                 routeEdge = edgesMap[routeEdge.previousEdgeId];
             }
 
@@ -70,17 +77,62 @@ ANYWAYS.tree = {
             }).addTo(map));
         };
 
-        var gradient2 = function  (color1, color2, ratio) {
-            var hex = function (x) {
-                x = x.toString(16);
-                return (x.length == 1) ? '0' + x : x;
-            };
+        //var gradient2 = function  (color1, color2, ratio) {
+        //    var hex = function (x) {
+        //        x = x.toString(16);
+        //        return (x.length == 1) ? x + x : x;
+        //    };
 
-            var r = Math.ceil(parseInt(color1.substring(0, 2), 16) * ratio + parseInt(color2.substring(0, 2), 16) * (1 - ratio));
-            var g = Math.ceil(parseInt(color1.substring(2, 4), 16) * ratio + parseInt(color2.substring(2, 4), 16) * (1 - ratio));
-            var b = Math.ceil(parseInt(color1.substring(4, 6), 16) * ratio + parseInt(color2.substring(4, 6), 16) * (1 - ratio));
+        //    var r = Math.ceil(parseInt(color1.substring(0, 2), 16) * ratio + parseInt(color2.substring(0, 2), 16) * (1 - ratio));
+        //    var g = Math.ceil(parseInt(color1.substring(2, 4), 16) * ratio + parseInt(color2.substring(2, 4), 16) * (1 - ratio));
+        //    var b = Math.ceil(parseInt(color1.substring(4, 6), 16) * ratio + parseInt(color2.substring(4, 6), 16) * (1 - ratio));
 
-            return hex(r) + hex(g) + hex(b);
+        //    return hex(r) + hex(g) + hex(b);
+        //};
+
+        var getGradientColor = function (start_color, end_color, percent) {
+            // strip the leading # if it's there
+            start_color = start_color.replace(/^\s*#|\s*$/g, '');
+            end_color = end_color.replace(/^\s*#|\s*$/g, '');
+
+            // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+            if (start_color.length == 3) {
+                start_color = start_color.replace(/(.)/g, '$1$1');
+            }
+
+            if (end_color.length == 3) {
+                end_color = end_color.replace(/(.)/g, '$1$1');
+            }
+
+            // get colors
+            var start_red = parseInt(start_color.substr(0, 2), 16),
+                start_green = parseInt(start_color.substr(2, 2), 16),
+                start_blue = parseInt(start_color.substr(4, 2), 16);
+
+            var end_red = parseInt(end_color.substr(0, 2), 16),
+                end_green = parseInt(end_color.substr(2, 2), 16),
+                end_blue = parseInt(end_color.substr(4, 2), 16);
+
+            // calculate new color
+            var diff_red = end_red - start_red;
+            var diff_green = end_green - start_green;
+            var diff_blue = end_blue - start_blue;
+
+            diff_red = ((diff_red * percent) + start_red).toString(16).split('.')[0];
+            diff_green = ((diff_green * percent) + start_green).toString(16).split('.')[0];
+            diff_blue = ((diff_blue * percent) + start_blue).toString(16).split('.')[0];
+
+            // ensure 2 digits by color
+            if (diff_red.length == 1)
+                diff_red = '0' + diff_red
+
+            if (diff_green.length == 1)
+                diff_green = '0' + diff_green
+
+            if (diff_blue.length == 1)
+                diff_blue = '0' + diff_blue
+
+            return '' + diff_red + diff_green + diff_blue;
         };
 
         var gradient = function (ratio) {
@@ -88,7 +140,12 @@ ANYWAYS.tree = {
             //{
             //    return gradient2(color1, color2, (ratio - 0.6) / 0.4);
             //}
-            return gradient2(color1, color3, ratio);
+            if (ratio >= 1) {
+                return color1;
+            } else if (ratio <= 0) {
+                return color3;
+            }
+            return getGradientColor(color3, color1, ratio);
         };
 
         var requestRoute = function (loc) {
@@ -105,6 +162,8 @@ ANYWAYS.tree = {
                     shadowUrl: 'images/marker-shadow.png'
                 })
             }).addTo(map);
+
+            me.index.clear();
 
             ANYWAYS.api.tree('http://analytics-api.anyways.eu/europe', {
                 profile: profile,
@@ -135,6 +194,17 @@ ANYWAYS.tree = {
                         if (idx < response.edges.length) {
                             if (requestId == localRequestId) {
                                 var edge = response.edges[idx];
+
+                                for (var s = 0; s < edge.shape.length - 1; s++) {
+                                    me.index.insert({
+                                        minX: Math.min(edge.shape[s][0], edge.shape[s + 1][0]),
+                                        minY: Math.min(edge.shape[s][1], edge.shape[s + 1][1]),
+                                        maxX: Math.max(edge.shape[s][0], edge.shape[s + 1][0]),
+                                        maxY: Math.max(edge.shape[s][1], edge.shape[s + 1][1]),
+                                        edgeId: edge.edgeId
+                                    });
+                                }
+
                                 var edgeLineString = {
                                     "type": "Feature",
                                     "properties": {
@@ -153,7 +223,10 @@ ANYWAYS.tree = {
                                         if (feature.properties && feature.properties.weight1) {
                                             var ratio = feature.properties.weight1 / max;
                                             var color = '#' + gradient(ratio);
-                                            var weight = 4 * (1 - ratio);
+
+                                            //console.log('ratio: ' + ratio + ' makes ' + color);
+
+                                            var weight = 2 * (1 - ratio);
                                             return {
                                                 weight: weight,
                                                 color: color,
@@ -161,13 +234,13 @@ ANYWAYS.tree = {
                                             };
                                         }
                                     },
-                                    onEachFeature: function (feature, layer) {
-                                        layer.on("mouseover", function (e) {
-                                            console.log('Mouseover on ' + me.profile);
-                                            me.report(feature.properties.edgeId);
-                                            me.selectEdge(feature.properties.edgeId);
-                                        });
-                                    }
+                                    //onEachFeature: function (feature, layer) {
+                                    //    layer.on("mouseover", function (e) {
+                                    //        //console.log('Mouseover on ' + me.profile);
+                                    //        me.report(feature.properties.edgeId);
+                                    //        me.selectEdge(feature.properties.edgeId);
+                                    //    });
+                                    //}
                                 }).addTo(map));
                             }
                         }
@@ -182,26 +255,12 @@ ANYWAYS.tree = {
                 };
                 animation();
 
-                //treeLayer = L.geoJson(response, {
-                //    style: function(feature) {
-                //        if (feature.properties && feature.properties.start_weight) {
-                //            var ratio = feature.properties.start_weight / max;
-                //            var color = '#' + gradient(ratio);
-                //            var weight = 4 * (1 - ratio);
-                //            return {
-                //                weight: weight,
-                //                color: color,
-                //                opacity: 1
-                //            };
-                //        }
-                //    }
-                //}).addTo(map);
             }, function (response, currentId, options) {
                 console.log('ERROR:' + response);
             }, this);
         };
 
-        map.on('click', function (e) {
+        me.onClick = function (e) {
             var loc = e.latlng;
 
             // removes the current layers.
@@ -221,7 +280,25 @@ ANYWAYS.tree = {
             idx = 1000000;
 
             requestRoute(loc);
-        });
+        };
+
+        me.onMouseMove = function (e) {
+            var loc = e.latlng;
+
+            var result = me.index.search({
+                minX: loc.lng - 0.001,
+                minY: loc.lat - 0.001,
+                maxX: loc.lng + 0.001,
+                maxY: loc.lat + 0.001
+            });
+
+            if (result.length > 0) {
+                me.selectEdge(result[0].edgeId);
+            }
+            //for (var i = 0; i < result.length; i++) {
+
+            //}
+        };
 
         requestRoute(start);
 
